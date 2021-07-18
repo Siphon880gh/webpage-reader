70354792d776ed83d0cb9a9b2aa0a428fd968174
Weng Fei Fung <weffung@ucdavis.edu>
2021-07-17 21:43:10 -0700

    More refined Wikipedia cleaning

--- profiles/wikipedia.js
+++ profiles/wikipedia.js
@@ -1,1 +1,2 @@
 $("#webpage-text").find("#toc, .mw-indicators, .mw-editsection, #mw-navigation, #Further_reading, #catlinks, [role='navigation'], #References, .refbegin, .reflist, #footer").remove()
+$("#webpage-text").find('[role="note"], #siteSub, .mw-jump-link').remove();
